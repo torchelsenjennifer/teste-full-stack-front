30eb2e50b018a8887e8ffeb37e922f574a99e8a1
Jennifer <jennifertorchelsen@gmail.com>
2023-02-25 00:21:04 -0300

    modificação do menu horizontal

---
+++ src/componentes/Menu/index.js
@@ -0,0 +1,13 @@
+import './Menu.css'
+
+const Menu = () => {
+  return (
+    <div className='menu'>
+          <img className="menu_logo" src="estoque.png" alt="Logo da agencia"  />
+          <h1 className='menu_titulo'>Estoque de Produtos e Categorias</h1>          
+          <h3 className='menu_descricao'>Stock aqui!</h3>
+    </div>
+  )
+}
+
+export default Menu
