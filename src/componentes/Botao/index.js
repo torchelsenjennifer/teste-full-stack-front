5718b0b01bd8d281c578109718854826cd000ca5
Jennifer <jennifertorchelsen@gmail.com>
2023-03-01 16:04:11 -0300

    estilizando botao e as informações do produto

--- src/componentes/Botao/index.js
+++ src/componentes/Botao/index.js
@@ -1,11 +1,11 @@
 
 import './Botao.css'
 
-const Botao = ({funcao, texto, cor_botao}) => {
+const Botao = ({funcao, texto, cor_botao, cor_letra, contorno}) => {
   return (
     <button 
-    style={{backgroundColor: cor_botao}}
-
+    className='botao'
+    style={{backgroundColor: cor_botao, color: cor_letra, borderRadius:contorno}}
     onClick={funcao }>
       {texto}
       </button>
--- src/componentes/Card/index.js
+++ src/componentes/Card/index.js
@@ -1,23 +1,28 @@
 import "./Card.css";
 import Botao from "../Botao";
 
-const Card = ({ foto, nome, preco, descricao, categoria }) => {
+const Card = ({ foto, nome, preco, descricao, categoria, cor_letra, contorno }) => {
   return (
     <div className="card">
       <img className="card_img" src={foto} alt="Produto" />
       <h4>{nome}</h4>
       <h5 className="card_valor">{`Valor: R$${preco}`}</h5>
       <h4>{descricao}</h4>
-      <h4 className="categoria">{categoria}</h4>
+      <h4 className="card_categoria">{categoria}</h4>
       <div className="botao">
         <Botao
           // funcao={() => history.push("/detalhes")}
           texto={"INFORMAÇÕES"}
-          cor_botao={"brown"}
+          cor_botao={"black"}
+          cor_letra={"white"}
+          contorno={'10px'}
+
         />
         <Botao
           texto={"ADICIONAR"}
-          cor_botao={"purple"}
+          cor_botao={"black"}
+          cor_letra={"white"}
+          contorno={'10px'}
         />
       </div>
     </div>
