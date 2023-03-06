8047c766fb36586a02268feae2c58aefbcb8ac99
Jennifer <jennifertorchelsen@gmail.com>
2023-03-06 20:20:46 -0300

    desenvolvendo context do carrinho

--- src/componentes/Menu/index.js
+++ src/componentes/Menu/index.js
@@ -1,13 +1,28 @@
-import './Menu.css'
+import "./Menu.css";
+import { BsCartCheck } from "react-icons/bs";
+import { useHistory } from 'react-router-dom';
+import { useCarrinhoContext } from "../../context/Carrinho"
 
 const Menu = () => {
+  const {quantidadeProdutos} = useCarrinhoContext();
+  const history = useHistory();
   return (
-    <div className='menu'>
-          <img className="menu_logo" src={process.env.PUBLIC_URL + "/assets/estoque.png"} alt="Logo da agencia"  />
-          <h1 className='menu_titulo'>Estoque de Produtos e Categorias</h1>          
-        
+    <div className="menu">
+      <img
+        className="menu_logo"
+        src={process.env.PUBLIC_URL + "/assets/estoque.png"}
+        alt="Logo da agencia"
+      />
+      <h1 className="menu_titulo">Estoque de Produtos e Categorias</h1>
+      <div className="foto_carrinho">
+        <BsCartCheck
+          size={50}
+          disabled={quantidadeProdutos === 0}
+          onClick={() => history.push("/carrinho")}
+        ></BsCartCheck>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Menu
+export default Menu;
