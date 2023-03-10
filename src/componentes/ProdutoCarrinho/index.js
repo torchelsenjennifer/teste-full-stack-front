061a53c5fd1b9e39aee014663869567d28cfd425
Jennifer <jennifertorchelsen@gmail.com>
2023-03-10 00:18:30 -0300

    estilazação da pagina carrinho

--- src/componentes/ProdutoCarrinho/index.js
+++ src/componentes/ProdutoCarrinho/index.js
@@ -1,9 +1,9 @@
-import Container from '@mui/material/Container';
-import RemoveIcon from '@mui/icons-material/Remove';
-import AddIcon from '@mui/icons-material/Add';
+// import Container from "@mui/material/Container";
+import RemoveIcon from "@mui/icons-material/Remove";
+import AddIcon from "@mui/icons-material/Add";
 import IconButton from "@mui/material/IconButton";
 import { useCarrinhoContext } from "../../context/Carrinho";
-import './ProdutoCarrinho.css'
+import "./ProdutoCarrinho.css";
 
 const ProdutoCarrinho = ({ foto, nome, preco, id }) => {
   const { carrinho, adicionarProduto, removerProduto } = useCarrinhoContext();
@@ -12,30 +12,30 @@ const ProdutoCarrinho = ({ foto, nome, preco, id }) => {
   );
 
   return (
-    <Container className='fundo' >
-    <div className='produto' >
-      <img className='produto_imagem' src={foto} alt="Produto" />
-      <p>
-        {nome} - R$ {preco?.toFixed(2)} <span>Kg</span>
-      </p>
+    <div className="produto">
+      <div className=" produto--info">
+        <img className="produto--info--imagem" src={foto} alt="Produto" />
+        <p className="produto--info--p">
+          {nome} - R$ {preco?.toFixed(2)} <span>Kg</span>
+        </p>
+      </div>
+      <div className="produto--quantidade">
+        <IconButton
+          color="secondary"
+          onClick={() => removerProduto(id)}
+          disabled={!produtoNoCarrinho}
+        >
+          <RemoveIcon />
+        </IconButton>
+        {produtoNoCarrinho?.quantidade || 0}
+        <IconButton
+          color="primary"
+          onClick={() => adicionarProduto({ nome, foto, id, preco })}
+        >
+          <AddIcon />
+        </IconButton>
+      </div>
     </div>
-    <div className='soma'>
-      <IconButton 
-        color="secondary"
-        onClick={() => removerProduto(id)}
-        disabled={!produtoNoCarrinho}
-      >
-        <RemoveIcon />
-      </IconButton>
-      {produtoNoCarrinho?.quantidade || 0}
-      <IconButton
-        color="primary"
-        onClick={() => adicionarProduto({ nome, foto, id, preco })}
-      >
-        <AddIcon />
-      </IconButton>
-    </div>
-  </Container>
   );
 };
 
